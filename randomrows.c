6e40806834abc2eb5285008306becdeebf781f92
Simona Nisticò <simonanisticokr@gmail.com>
2019-05-03 12:01:51 +0200

    Sistemata la sintassi, fatti miglioramenti per diminuire l'utilizzo di strutture dati superflue

--- randomrows.c
+++ randomrows.c
@@ -3,43 +3,59 @@
 #include <math.h>
 #include <stdlib.h>
 
+#define	MATRIX		double*
+#define	VECTOR		double*
+
 int n;
 int k;
-int kc; 
+int kc;
 
 //Matrice che ogni riga rappresenta un punto e ogni colonna un centroide
 //L'elemento m[i][j] rappresenta la distanza del punto i dal centroide j
-double** distancesFromCentroids; 
+MATRIX distancesFromCentroids;
 
 //Punti del dataset da sosituire con quelli veri
-double* punti;
+VECTOR ds;
 
 //Array con l'indice dei punti che alla prima iterazione sono anche i centroidi
-int* centroidiCasuali;
+VECTOR centroidiCasuali
 
 //Centroidi veri e propri da codificare come i punti
-double** centroids;
+MATRIX centroids;
 
-//Matrice di due colonne che per ogni indice i che rappresenta il punto corrispondente
-//memorizza in [i][0] il centroide corrispondendente e in [i][1] la distanza dal centroide
-double** centroideDelPunto;
 
 //genera k numeri casuali diversi da 0 a n incluso
-int* generate(int n, int k){
+void generate_centroids(int n, int k){
 
-	int* numbers = calloc(k,sizeof(int));
+	int number,old_number,i,j;
+
+	// Alloco la matrice dei centroidi
+	centroids = alloc_matrix(k,d);
 
 	srand(time(NULL));
-	numbers[0]=rand()%(n/k);
-	printf("%d\n",numbers[0]);
-	
+	old_number=rand()%(n/k);
+	printf("%d\n",old_number);
+
+	// Inseriamo il centroide nella matrice
+	for(j=0;j<d;j++){
+		centroids[i*d+j] = ds[old_number*d+i];
+	}
+
 	int range;
-	for(int i=1;i<k;i++){
-	
-		range = (n-numbers[i-1])/(k-i);
+	for(i=1;i<k;i++){
+
+		range = (n-old_number)/(k-i);
 		printf("Range: %d  ",range );
-  		numbers[i]=numbers[i-1]+rand()%range+1;
+  	number=old_number+rand()%range+1;
 		printf("%d\n",numbers[i]);
+
+		// Inseriamo il centroide nella matrice
+		for(j=0;j<d;j++){
+			centroids[i*d+j] = ds[number*d+i];
+		}
+
+		old_number = number;
+
  	}
 
 //Mettere nella matrice centroids i punti corrispondenti agli indici trovati
@@ -47,44 +63,56 @@ int* generate(int n, int k){
 }
 
 // la matrice distances deve essere creata una volta sola visto che il metodo seguente deve stare dentro un ciclo
-double** computeDistancesFromCentroids(){
+void computeDistancesFromCentroids(){
 
-        double** distances = calloc(n,sizeof(double));
-	double minDist;
+	VECTOR punto = alloc_matrix(1,k);	// Alloco il vettore che conterrà di volta in volta il punto
+	double minDist,distanza;
 	int minCentr;
-	for(int i=0;i<n;i++){
-		distances[i]=calloc(kc,sìzeof(double));
-		distances[i][0] = distance((punti+i*d)*, centroids[0]);
-		minDist = distances[i][0];
-		minCentr = 0; 
-		for(int j=1;j<kc;j++){ //Verificare la sintassi, l'obiettivo è passare l'indirizzo di partenza
+	int i,j,k;
+
+	for(i=0;i<n;i++){
+
+		for(k=0;k<d;k++){
+			punto[k] = ds[d*i+k];	// Sto supponendo che il dataset sia memorizzato per righe
+		}
+
+		minDist = distance(punto, centroids[0]);
+		minCentr = 0;
+
+		for(j=1;j<kc;j++){ //Verificare la sintassi, l'obiettivo è passare l'indirizzo di partenza
 					//del punto i-esimo
-			distances[i][j]=distance((punti+i*d)*, centroids[j]);
-			if(distances[i][j] < minDist){
-				minDist = distances[i][j];
+
+			for(k=0;k<d;k++){
+				punto[k] = ds[d*i+k];
+			}	// Sto supponendo che il dataset sia memorizzato per righe
+
+			distanza=distance(punto, centroids[j]);
+
+			if(distanza < minDist){
+				minDist = distanza;
 				minCentr = j;
 			}
 
 		}
-		
+
 		centroideDelPunto[i][0] = minCentr;
 		centroideDelPunto[i][1] = minDist;
 	}
-	return distances;
+
+	// Liberiamo lo spazio (verificare se si può utilizzare la stessa funzione della matrice)
+	dealloc_matrix(punto);
 
 }
 
-float* centroids(int* starting);
-
 //Si può applicare il loop vectorization cioè più operazioni nello stesso ciclo
 //Oppure più punti alla volta
-double distance(int* puntoX, int* puntoY){ 
+double distance(VECTOR puntoX, VECTOR puntoY){
 
 	double sum=0;
 	double diff=0;
 	for(int i=0;i<d;i++){
 		diff = (puntoX[i]-puntoY[i]);
-		sum+= diff*diff;	
+		sum+= diff*diff;
 	}
 	return sqrt(sum);
 }
