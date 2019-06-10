5e5f98c9bf9a707263c73a66bc4292ecaadb3b73
Fabio <fabioz96@hotmail.it>
2019-06-10 12:15:10 +0200

    sscript e tempistiche

---
+++ script_tempi.sh
@@ -0,0 +1,15 @@
+
+./runpqnn32
+
+./pqnn32c prova -exaustive -adc -knn 4
+./pqnn32c prova -s -exaustive -sdc -knn 4
+./pqnn32c prova -s -noexaustive -adc -knn 4
+./pqnn32c prova -s -noexaustive -sdc -knn 4
+
+
+./runpqnn64
+
+./pqnn64c prova -s -exaustive -adc -knn 4
+./pqnn64c prova -s -exaustive -sdc -knn 4
+./pqnn64c prova -s -noexaustive -adc -knn 4
+./pqnn64c prova -s -noexaustive -sdc -knn 4
